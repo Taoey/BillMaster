98b163a72a95dd5a74a2d4caeb722e07b9840668
Taoey <741494582@qq.com>
2019-07-02 12:01:27 +0800

    添加bill_detail_ali表

---
+++ db/查询SQL.sql
@@ -0,0 +1,10 @@
+
+
+-- 创建bill_detail_ali
+
+ALTER TABLE `bill_detail_ali`
+ADD COLUMN bill_place VARCHAR(50) DEFAULT "" COMMENT '交易来源地'
+ADD COLUMN bill_type SMALLINT(1) DEFAULT 0 COMMENT '交易类型 1-即时到账交易 2-支付宝担保交易'
+ADD COLUMN trader VARCHAR(100) DEFAULT "" COMMENT '交易对方'
+ADD COLUMN goods VARCHAR(100) DEFAULT '' COMMENT '商品名称'
+ADD COLUMN pay_num BIGINT(20) DEFAULT 0 COMMENT '账单支出'
